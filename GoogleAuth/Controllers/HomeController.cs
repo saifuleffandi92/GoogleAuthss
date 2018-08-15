8b82510994d27a4c56001eefa635b4ef3d5935a5
saifuleffandi92 <saifuleffandi92@gmail.com>
2018-08-15 16:56:28 +0800

    Add project files.

---
+++ GoogleAuth/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Configuration;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using GoogleAuth.Models;
+
+namespace GoogleAuth.Controllers {
+    public class HomeController : Controller {
+
+        private string ClientId = ConfigurationManager.AppSettings["Google.ClientID"];
+        private string SecretKey = ConfigurationManager.AppSettings["Google.SecretKey"];
+        private string RedirectUrl = ConfigurationManager.AppSettings["Google.RedirectUrl"];
+
+        public async Task<ActionResult> Index() {
+            string token = (string)Session["user"];
+            if (string.IsNullOrEmpty(token)) {
+                return View();
+            }
+            else {
+                return View("UserProfile", await GetuserProfile(token));
+            }
+        }
+
+        public void LoginUsingGoogle() {
+            Response.Redirect($"https://accounts.google.com/o/oauth2/v2/auth?client_id={ClientId}&response_type=code&scope=openid%20email%20profile&redirect_uri={RedirectUrl}&state=abcdef");
+        }
+
+        [HttpGet]
+        public ActionResult SignOut() {
+            Session["user"] = null;
+            return View("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> SaveGoogleUser(string code, string state, string session_state) {
+            if (string.IsNullOrEmpty(code)) {
+                return View("Error");
+            }
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.googleapis.com")
+            };
+            var requestUrl = $"oauth2/v4/token?code={code}&client_id={ClientId}&client_secret={SecretKey}&redirect_uri={RedirectUrl}&grant_type=authorization_code";
+
+            var dict = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/x-www-form-urlencoded" }
+            };
+            var req = new HttpRequestMessage(HttpMethod.Post, requestUrl) { Content = new FormUrlEncodedContent(dict) };
+            var response = await httpClient.SendAsync(req);
+            var token = JsonConvert.DeserializeObject<GmailToken>(await response.Content.ReadAsStringAsync());
+            Session["user"] = token.AccessToken;
+            var obj = await GetuserProfile(token.AccessToken);
+
+            //IdToken property stores user data in Base64Encoded form  
+            //var data = Convert.FromBase64String(token.IdToken.Split('.')[1]);  
+            //var base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data);  
+
+            return View("UserProfile", obj);
+        }
+
+        public async Task<UserProfile> GetuserProfile(string accesstoken) {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.googleapis.com")
+            };
+            string url = $"https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token={accesstoken}";
+            var response = await httpClient.GetAsync(url);
+            return JsonConvert.DeserializeObject<UserProfile>(await response.Content.ReadAsStringAsync());
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GoogleAuth/Models/UserProfile.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace GoogleAuth.Models {
+    public partial class UserProfile {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("verified_email")]
+        public bool VerifiedEmail { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("given_name")]
+        public string GivenName { get; set; }
+
+        [JsonProperty("family_name")]
+        public string FamilyName { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
+    }
+
+    public class GmailToken {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public long ExpiresIn { get; set; }
+
+        [JsonProperty("id_token")]
+        public string IdToken { get; set; }
+    }
+}
